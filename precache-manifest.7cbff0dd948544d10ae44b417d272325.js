8bca7003130a53deac63045d02c2ba3113034d38
saichinmay2000 <saichinmayt@gmail.com>
2020-06-27 21:30:57 +0530

    Updates

---
+++ precache-manifest.7cbff0dd948544d10ae44b417d272325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcccc1cdca24d71d53a7a31c6ce8e67a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ec4ac88aa0becf76ce",
+    "url": "/static/css/main.c6a8e4dd.chunk.css"
+  },
+  {
+    "revision": "2fa07d532e0230538775",
+    "url": "/static/js/2.5a0fc613.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a0fc613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ec4ac88aa0becf76ce",
+    "url": "/static/js/main.a997dbb6.chunk.js"
+  },
+  {
+    "revision": "9a02a2c455c0d30fdada",
+    "url": "/static/js/runtime-main.e9291dc7.js"
+  },
+  {
+    "revision": "a4b3030dc6c3c0fc8b2aaad1f1233e02",
+    "url": "/static/media/banner_img.a4b3030d.png"
+  }
+]);
