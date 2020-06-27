18650a190629ac8b51480bfe4b44c4fc83b31e4e
saichinmay2000 <saichinmayt@gmail.com>
2020-06-27 21:19:33 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import '../src/App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Features from './components/Features';
+import Team from './components/Team';
+import Process from './components/Process';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Process />
+      <Team />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Features.js
@@ -0,0 +1,142 @@
+import React from 'react';
+class Features extends React.Component{
+    render()
+    {
+        return(
+            <section class="text-gray-500 bg-gray-900 body-font" id="features">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="text-center mb-20">
+      <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-white mb-4">Expertise @</h1>
+      <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">We keep upgrading to the latest Tech as per the updates</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-white mb-4 text-sm text-center sm:text-left">Web Development</h2>
+        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1">
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>MEAN / MERN Stack Development
+          </a>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>PHP
+          </a>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Python-Django/Flask
+          </a>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Bootstrap 5
+          </a>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Wordpress
+          </a>
+        </nav>
+      </div>
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-white mb-4 text-sm text-center sm:text-left">Cross Platform App Development</h2>
+        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1">
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>React Native
+          </a>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Flutter
+          </a><div class="p-4 lg:w-1/4 sm:w-1/2 w-full"/>
+        <h2 class="font-medium title-font tracking-widest text-white mb-4 text-sm text-center sm:text-left">Native App Development</h2>
+        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1"/>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Java - Android App Development
+          </a>
+        </nav>
+      </div>
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-white mb-4 text-sm text-center sm:text-left">UI/UX Design</h2>
+        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1">
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>App Prototype &amp; Design
+          </a>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Web Prototype &amp; Design
+          </a>
+          <div class="p-4 lg:w-1/4 sm:w-1/2 w-full"/>
+        <h2 class="font-medium title-font tracking-widest text-white mb-4 text-sm text-center sm:text-left">Deployment Services</h2>
+        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1"/>
+        <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Linux - NGNIX/Apache
+        </a>
+        <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Cloud Deployment - PaaS
+        </a>
+        </nav>
+      </div>
+      <div class="p-4 lg:w-1/4 sm:w-1/2 w-full">
+        <h2 class="font-medium title-font tracking-widest text-white mb-4 text-sm text-center sm:text-left">Artificial Intelligence</h2>
+        <nav class="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1">
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>AI integrated Web Development
+          </a>
+          <a class="mb-2">
+            <span class="bg-gray-800 text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Predictions based Data Analytics
+          </a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</section>
+        )
+    }
+}
+export default Features;
